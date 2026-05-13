fc0c2880c3bdd911563870a9ce304add8d3f5b3c
w1l <121729539+OvrL0rd@users.noreply.github.com>
2026-05-13 15:02:39 -0400

    Updated eslint config

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -25,6 +25,9 @@ export default defineConfig([
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/purity': 'off',
+      'react-hooks/immutability': 'off',
+      'react-hooks/preserve-manual-memoization': 'off',
     },
   },
 ])
