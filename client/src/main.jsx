467ed8f85f6f81a76dec35e43cf9fcafc7e41a17
w1l238 <wdooley15@outlook.com>
2026-06-09 13:58:08 -0400

    refactor: wrap app with library provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
+import { initCsrf } from './utils/apiClient';
+
+initCsrf();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
